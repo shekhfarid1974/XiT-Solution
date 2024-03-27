5a1d9b4798fee452c8303f2a46738f28d5b84df6
shekh farid <shekhfarid1974@gmail.com>
2024-03-27 11:49:02 +0600

    task1

---
+++ Task-1/facebook.js
@@ -0,0 +1,19 @@
+
+class="x1lliihq x6ikm8r x10wlt62 x1n2onr6 xlyipyv xuxw1ft"
+
+class="x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen x1s688f x1dem4cn"
+
+setInterval (() => { const allbutton = document.getElementByClassName("xuxw1ft"); for(const item of allbutton) if (item.innerText == "Cancel request") {item.click()}},2000)
+
+<script>
+  setInterval(() => {
+      const allButtons = document.getElementsByClassName("xuxw1ft");
+      for (const item of allButtons) {
+          if (item.innerText == "Cancel request") {
+              item.click();
+          }
+      }
+  }, 2000);
+</script>
+
+
